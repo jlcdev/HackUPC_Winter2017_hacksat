2bfdea2020ae069cc067413fb6821ed9db187132
Javier López Calderón <potnox@gmail.com>
2017-03-05 08:55:27 +0100

    Add comments V1

--- SatTransmitter/src/main.ino
+++ SatTransmitter/src/main.ino
@@ -4,6 +4,7 @@ const int pin_telemetry = 3;
 
 void setup()
 {
+  //Set virtual wire config for send data over RFmodule
   vw_setup(2000);
   vw_set_tx_pin(pin_telemetry);
   Serial.begin(9600);
@@ -12,6 +13,7 @@ void setup()
 
 void loop(){
   if(Serial.available() > 0){
+    //Send 64 Bytes chunks of data from experiment, SAT only serves as a proxy
     uint8_t content[Serial.available()];
     Serial.readBytes(content, Serial.available());
     vw_send((uint8_t *)content, strlen(content));
