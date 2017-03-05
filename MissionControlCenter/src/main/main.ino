ce23944ed64432cb16fe803aa961c59a12a394a8
Javier López Calderón <potnox@gmail.com>
2017-03-05 09:30:00 +0100

    CHANGE comments V2

--- MissionControlCenter/src/main/main.ino
+++ MissionControlCenter/src/main/main.ino
@@ -1,23 +1,24 @@
 #include <VirtualWire.h>
 void setup()
 {
-  Serial.begin(9600); //Se inicia la comunicaciÃ³n serial
+  //Init fake receiver configuration
+  Serial.begin(9600);
   vw_set_ptt_inverted(true);
-  vw_setup(2000); //Tiempo de espera
-  vw_set_rx_pin(3); //Pin donde se conecta el Arduino
-  vw_rx_start(); //Se inicia la recepcin de datos
+  vw_setup(2000);
+  vw_set_rx_pin(3); //Pin for connect antenna with arduino
+  vw_rx_start();
 }
 void loop()
 {
-  uint8_t buf[VW_MAX_MESSAGE_LEN]; //TamaÃ±o del mensaje
+  uint8_t buf[VW_MAX_MESSAGE_LEN]; //Set buffer max size
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
-  if (vw_get_message(buf, &buflen)) //Se verifica si hay //mensaje disponible para ser leido
+  if (vw_get_message(buf, &buflen)) //If new message from SAT -> stores and prints to serial port
   {
     int i;
-    for (i = 0; i < buflen; i++) //Se leen todos los caracteres
+    for (i = 0; i < buflen; i++)
     {
-      Serial.write(buf[i]); //Se guarda la informaciÃ³n en una //matriz
+      Serial.write(buf[i]);
     }
-    Serial.println(); //Se imprime el mensaje en el monitor //serial
+    Serial.println();
   }
 }
